9b79827806eb8365b3e17e472f3d7018fae6ce53
VardgesHambaryan <vardges.hambaryan@gmail.com>
2023-05-04 22:05:37 +0400

    Comment

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello')
+s='a,2,b,5,c,8,a,4,e,11'
+s=s.split(',')
+answer={}
+for i in range(0,len(s),2):
+    if s[i] not in answer:
+        answer[s[i]]=int(s[i+1])
+    else:
+        answer[s[i]]+=int(s[i+1])
+print(answer)
+
+
+
+
